08ad2f6ce33eb9c3b33972e45ed056b43f15b703
Alice Xiang <alicex84@gmail.com>
2014-11-02 03:23:55 -0500

    Change page names

--- public/modules/articles/config/articles.client.config.js
+++ public/modules/articles/config/articles.client.config.js
@@ -5,7 +5,7 @@ angular.module('articles').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Proposals', 'articles', 'dropdown', '/articles(/create)?');
-		Menus.addSubMenuItem('topbar', 'articles', 'List Proposals', 'articles');
-		Menus.addSubMenuItem('topbar', 'articles', 'New Proposals', 'articles/create');
+		Menus.addSubMenuItem('topbar', 'articles', 'List of Policy Proposals', 'articles');
+		Menus.addSubMenuItem('topbar', 'articles', 'Create a New Proposal', 'articles/create');
 	}
 ]);
